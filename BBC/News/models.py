f271c455510aeae842c17d0985b19a74c57d8096
jidealakija <francisalakija@gmail.com>
2023-11-06 23:39:45 +0100

    2nd commit

--- BBC/News/models.py
+++ BBC/News/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    headline = models.CharField(max_length = 100)
+    content = models.TextField(max_length=5000)
+    reporter = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to= 'product_images')
+    date_reported = models.DateField()
+
+
+    class Meta:
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.name
--- BBC/News/views.py
+++ BBC/News/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
+def homepage(request):
+    all_news = News.objects.all()
+    context = {'all_news': all_news}
+    return render(request, 'news/index.html', context)
